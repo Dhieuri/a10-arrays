13c69e6da9aeb769fa82515333da5bd2adc0db79
Dhieuri <dhieuri@gmail.com>
2022-07-28 00:09:42 -0300

    Exercicio 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,20 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const num = [32, 24, 96, 105];
+const objeto = ['cateira','controle','caneta','esqueiro'];
+const misto = [312, 'flamengo', true];
+
+console.log(num);
+console.log(objeto);
+console.log(misto);
+
+//execicio 2, parte 1.
+console.log('Quantidade de itens Num =', num.length);
+console.log('Quantidade de itens Objeto =', objeto.length);
+console.log('Quantidade de itens Misto =', misto.length);
+
+//execicio 2, parte 2.
+console.log("Primeiro item de Num =", num[1]);
+console.log('Segundo item de Objetos =', objeto[2]);
+
+//exercicio 2, parte 3
+console.log(num.includes(96))
+console.log(misto.includes("café"))
